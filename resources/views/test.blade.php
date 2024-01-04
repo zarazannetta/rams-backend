1773fdc4097787b8d33be1f6c3ee6beaffdc58d9
OneStyd <wawansetyadi33@gmail.com>
2024-01-04 19:56:54 +0700

    Setup Project

---
+++ resources/views/test.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RAMS</title>
+</head>
+<body>
+    Test
+</body>
+</html>
