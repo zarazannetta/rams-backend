0229a6d02c7497fbf9404b39bb909fb6f35b36a0
OneStyd <wawansetyadi33@gmail.com>
2024-07-20 13:36:59 +0700

    Fix login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
         $token = $user->createToken('User Logged In')->plainTextToken;
 
         return response()->json([
-            'nama' => Auth::user()->fullname,
+            'nama' => $user->fullname,
             'token' => $token
         ]);
     }
