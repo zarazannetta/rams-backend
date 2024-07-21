4445990b178e9771590ed16353695f4d961994e0
OneStyd <wawansetyadi33@gmail.com>
2024-07-21 23:16:41 +0700

    Test Get Leger 3

--- app/Http/Controllers/LegerController.php
+++ app/Http/Controllers/LegerController.php
@@ -11,7 +11,27 @@ class LegerController extends Controller
 {
     public function getData($kode_leger)
     {
-        $leger_jalan = LegerJalan::with('dataJalanIdentifikasi')
+        $leger_jalan = LegerJalan::with(
+                                'dataJalanIdentifikasi', 
+                                'dataJalanTeknik1',
+                                'dataJalanTeknik2Lapis',
+                                'dataJalanTeknik2Median',
+                                'dataJalanTeknik2BahuJalan',
+                                'dataJalanTeknik3Goronggorong',
+                                'dataJalanTeknik3Saluran',
+                                'dataJalanTeknik3Bangunan',
+                                'dataJalanTeknik4',
+                                'dataJalanTeknik5Utilitas',
+                                'dataJalanTeknik5Bangunan',
+                                'dataJalanLHR',
+                                'dataJalanGeometrik',
+                                'dataJalanLingkungan',
+                                'dataJalanLainnya',
+                                'kodeProvinsi', 
+                                'kodeKabkot', 
+                                'kodeKecamatan', 
+                                'kodeDesa'
+                            )
                             ->where('kode_leger', $kode_leger)->first();
         return response()->json($leger_jalan);
     }
